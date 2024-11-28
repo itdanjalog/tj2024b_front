5196993d61dd6b4bb09a4e2b057f3c697a7f8c26
itdanjalog <kgs2072@naver.com>
2024-11-28 13:20:44 +0900

    day13

--- day14/1_중간과제8.js
+++ day14/1_중간과제8.js
@@ -97,7 +97,30 @@ function 등록함수( ){  console.log('등록함수 실행');
 } // f end 
 
 // [3] 출력함수  , 실행조건 : js열렸을때최초1번 실행 , 등록/삭제/수정 처리 성공시 실행 
-function 출력함수( ){ }
+출력함수( );
+function 출력함수( ){
+    // (1) 어디에 , table > tbody ,  document.querySelector(선택자)
+    let tbody = document.querySelector( 'table > tbody')
+    // (2) 무엇을 , 배열 요소들의 정보를 html 구성해서 
+    let html = ''
+        // - 배열내 요소 순회( 배열내 모든 요소를 반복해서 하나씩 꺼내기 )
+        for( let index = 0 ; index <= 게시물목록.length - 1 ; index++ ){
+            // index는 0부터 마지막인덱스까지 1씩 증가 반복 
+            let board = 게시물목록[index]; // 하나의 게시물
+            // 하나의 게시물을 csv 구성 했기 때문에 게시물정보 분해
+            // 문자열.split('기준문자') : 문자열내 기준문자으로 분해해서 분해된 결과를 배열 반환 함수
+            let info = board.split(',') // csv 형식은 ,(쉼표)로 구분했기 때문에 ,(쉼표) 다시 분해한다.
+                // info[0] = 제목 , info[1] = 내용 , info[2] = 비밀번호 , info[3] = 날짜 , info[4] = 조회수 
+            // 각 정보들을 HTML 과 연동해서 작성하기 
+            html += `<tr>
+                        <td> ${ info[3] } </td>
+                        <td> ${ info[0] } </td>
+                        <td> ${ info[4] } </td>
+                    </tr>` // 변수 += 값  vs 변수 = 변수 + 값  , 기존변수에 새로운 값을 누계/연결 
+        } // for end 
+    // (3) 출력 ,.innerHTML
+    tbody.innerHTML = html; // 변수 = 새로운값 ,  .변수 = 새로운값 
+} // f end 
 
 // [4] 삭제함수  , 실행조건 : [삭제]버튼 클릭시 
 function 삭제함수( ){ }
@@ -106,5 +129,7 @@ function 삭제함수( ){ }
 function 수정함수( ){ }
 
 
+let age = 10;
 
+console.log( age );
 
