2933cdd58692ff7ff6fb649489bde9e3cbad5a9b
itdanjalog <kgs2072@naver.com>
2024-11-28 12:57:37 +0900

    day13

--- day14/1_중간과제8.js
+++ day14/1_중간과제8.js
@@ -73,11 +73,18 @@ function 등록함수( ){  console.log('등록함수 실행');
     let content = document.querySelector('.content').value;   //console.log( content );
     let password = document.querySelector('.password').value;  //console.log( password );
             // 오류 : Cannot read properties of null (reading 'value')
-            // ---> HTML과 JS에 입력한 선택자가 일치하지 않을경우 , value속성이 없는 마크업 , 오타
+            // 이유 : HTML과 JS에 입력한 선택자가 일치하지 않을경우 , value속성이 없는 마크업 , 오타
     // 2. 처리 ,  입력받은값들과 날짜/조회수 하나의 문자열(CSV)구성 -> 배열 저장 .push
         // (1) 사용자에게 입력받지 않고 로직에서 초기화 해주는 변수
-    let date = '2024-11-28';// 작성일, 일반적으로 게시물 등록시 현재 시스템 날짜를 사용.
+    // 원하는 날짜 또는 시간 구성하기 
+        // - 현재 날짜/시간 기능를 제공하는 객체를 변수에 저장 
+    let nowDate = new Date()  // 현재 날짜/시간 제공하는 객체 생성 
+    let nowYear = nowDate.getFullYear() // 현재 연도 반환 함수
+    let nowMonth = nowDate.getMonth()+1; // 현재 월 반환 함수 , +1 , 0(1월) 11(12월)
+    let nowDay = nowDate.getDate() // 현재 일 반환 함수 
+    let date = `${nowYear}-${nowMonth}-${nowDay}`;  // 작성일, 일반적으로 게시물 등록시 현재 시스템 날짜를 사용.
     //console.log( date );
+    
     let view = 0; // 조회수 , 일반적으로 게시물 등록시 게시물 조회수는 0부터 시작 
     //console.log( view );
         // (2) 5개의 변수들을 하나의(CSV형식)문자열 로 구성
