b1a3a10272572d8e7bfd303766e691c1de03f3eb
itdanjalog <kgs2072@naver.com>
2024-12-02 16:24:54 +0900

    day16

--- day16/5_객체활용.js
+++ day16/5_객체활용.js
@@ -45,6 +45,7 @@ console.log( 평가목록 ); // 평가목록내 세번째 위치한 점수는 �
 
 //[*] 사원번호 전역변수 
 let eno = 3; // <--- 샘플 2개(사원2개)라서 다음 등록된 사원번호는 3번부터 시작
+
 //[1] 사원 등록 함수 , 실행조건 : [사원등록] 버튼 클릭할때 실행  
 function 사원등록(){
     // 1. 입력 , 각 선택자(클래스)와 일치한 input의 value값 가져오기 
@@ -62,6 +63,7 @@ function 사원등록(){
     // 3. 출력 
     사원전체출력();
 }// f end 
+
 // [2] 사원 정보 전체 출력 함수 , 실행조건 : 1. JS실행될때 최초1번 2.사원목록(배열)의 변화(저장/수정/삭제)가 있을때 마다
 사원전체출력();
 function 사원전체출력(){
@@ -74,11 +76,55 @@ function 사원전체출력(){
                     <td> ${ info.사원번호 }</td>
                     <td> ${ info.사원명 } </td>
                     <td> ${ info.생년월일 } </td>
+                    <td> 
+                        <button onclick="평가등록출력( ${ info.사원번호 } )" type="button">평가등록</button>
+                        <button type="button">평가결과</butoon>
+                    </td>
                 </tr>`
     } // f end 
     // 3. 출력 
     tbody.innerHTML = html;
 } // f end 
+// [3] 평가 등록 구역 출력 함수 , 실행조건 : [평가등록] 버튼을 클릭했을때
+function 평가등록출력( 클릭된사원번호 ){ // 매개변수 : 클릭된사원번호
+    // 1.클릭된사원번호를 이용한 사원정보 찾기.
+    let 검색사원 = null; // 2.'검색사원' 변수 선언하여 검색 결과를 저장할 변수 // null 객체가 없다는 뜻 
+    for( let index = 0 ; index <= 사원목록.length-1 ; index++ ){ // 3. index는 0부터 마지막인덱스까지 1씩증가 반복
+        let info = 사원목록[index]; // 4. index번째의 사원정보(객체) 호출 
+        if( info.사원번호 == 클릭된사원번호 ){ // 5. 만일 index번째의 사원번호가 클릭한사원번호와 같으면
+            검색사원 = info; // 6. for 밖에서 선언한 '검색사원' 변수에 찾은 객체를 대입한다.
+            break; // 7. 만일 찾았으면 for문 종료
+        }// if end 
+    } // f end 
+    if( 검색사원 == null ){ // 8. 만일 검색사원의 값이 null 이면 못찾았다. 찾았다면 사원정보(객체{})
+        alert('사원정보가 없습니다.'); 
+        return; // 9. 현재 함수 종료 
+    } // if end 
+    // 1. 어디에 
+    let div = document.querySelector('.평가입력구역')
+    // 2. 무엇을 
+    let html = `<h3> 사원 평가 등록 </h3>
+                <form>
+                    <h5> 평가사원명 : ${ 검색사원.사원명 } </h5>
+                    평가연도 : <input type="text" />
+                    상반기평가 : <input type="text" />
+                    하반기평가 : <input type="text"/> 
+                    <button type="button"> 점수 등록 </button>
+                </form>`
+    // 3, 출력 
+    div.innerHTML = html
+} // f end 
+
+
+
+
+
+
+
+
+
+
+
 
 
 
