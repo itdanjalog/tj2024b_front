02abf8b08b24242d231d72dbf89939b1f930f210
itdanjalog <kgs2072@naver.com>
2024-12-03 18:20:52 +0900

    day17

--- day17/4_중간과제13.js
+++ day17/4_중간과제13.js
@@ -15,7 +15,7 @@
             
             - 할일상태 : 할일상태는 '완료'=1/true 또는 '미완료'=0/false 로 표현할 생각
                 - true에 '완료' 의미 부여 , false에 '미완료' 의미 부여 
-                    --> 1. if 간소화 2. '완료'단어보다 true리터럴이 메모리가 더 적다.
+                    --> 1. if 간소화 2. '완료'단어보다 true리터럴이 메모리가 더 적다. 3. 부정연산자
             - 객체와 배열 샘플 구성 ( 표/테이블=배열1개 , 가로/행1개 = 객체1개)
         
         4. (JS) 기능/이벤트 구성 : 1.필요한 함수/기능 들을 파악 2.각 함수들이 실행되는조건 3. 각 함수들의 매개변수/리턴값 
@@ -79,7 +79,7 @@ function 전체출력함수(){
             html += `<div class="contentBox ${ info.할일상태 == true ? 'success' : '' }">  
                         <div class="content"> ${ info.할일내용 } </div>
                         <div class="contentBtns">
-                            <button class="updateBtn">수정</button>
+                            <button onclick="수정함수( ${ info.할일코드 })" class="updateBtn">수정</button>
                             <button onclick="삭제함수( ${ info.할일코드 } )" class="deleteBtn">삭제</button> 
                         </div>
                     </div>`
@@ -105,8 +105,16 @@ function 삭제함수( 삭제할일코드 ){
 } // 삭제 함수 end 
 
 //[4] 상태 수정 함수, ( p:할일코드 , r:x )
-function 수정함수(){ 
-    
+function 수정함수( 수정할일코드 ){ 
+    // 1. 배열내 수정할 객체/요소 를 찾는다. 
+    for( let index = 0 ; index <= 할일목록.length-1 ; index++ ){
+        if( 할일목록[index].할일코드 == 수정할일코드 ){
+            //만약에 index번째 할일객체내 할일코드가 수정할일코드와 같다면 
+            let 할일상태 = 할일목록[index].할일상태 // 할일상태를 호출
+            할일목록[index].할일상태 = !할일상태 // 할일상태를 부정후 다시 할일상태에 대입  false -> true , true -> false 
+            break; // 수정했다면 다음 객체 조회는 할 필요 없이 반복문 종료 
+        } // if end 
+    } // for end 
     전체출력함수();
     return; 
 }
