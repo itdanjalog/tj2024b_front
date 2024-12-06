135ddf5f9101b71dead260f46e02381fe5d994f0
itdanjalog <kgs2072@naver.com>
2024-12-06 13:07:12 +0900

    day20

--- day20/2_실습.js
+++ day20/2_실습.js
@@ -12,20 +12,7 @@ function 회원가입함수( ){
     // 2. [처리]
     let member = { 'id' : id , 'pw' : pw }; // 입력받은 값으로 객체 생성     
 
-        // * 회원목록정보를 전역변수에 선언하지 않고 세션/쿠키 호출 
-        // 왜? JS외부 (브라우저)에 저장하면 JS가 새로고침/변화가 있어도 저장 상태 유지 
-        // - (1) 현재 회원정보를 저장하기 위한 회원정보목록을 세션에서 가져오기.
-        let memberList = sessionStorage.getItem( 'memberList' );
-        console.log( memberList ); // 존재하지 않는 'key' 호출하면 null 반환된다.
-        // - (2) 회원정보목록이 존재하면 존재하지 않으면 
-        if( memberList == null ){
-            //만약에 가져온세션데이터(memberList)가 null(없다면) 이면
-            memberList = []; // 빈 배열 생성하여 변수에 대입한다. 
-        }else{// 가져온세션데이터(memberList) 가 존재하면 
-            // 문자열 ---> 객체(JSON) 으로 변환
-            memberList = JSON.parse( memberList );
-        }
-        console.log( memberList );
+        let memberList = 회원정보목록반환함수();
 
         // 배열에 입력받은 객체 저장 
         memberList.push( member ); console.log( memberList );
@@ -47,12 +34,8 @@ function 로그인함수(){
     let loginId = document.querySelector('.loginId').value;
     let loginPw = document.querySelector('.loginPw').value;
 
-    let memberList = sessionStorage.getItem('memberList');
-    if( memberList == null ){
-        memberList = [] // 회원정보목록이 없으면 빈배열 생성 
-    }else{// 아니면 
-        memberList = JSON.parse( memberList ); // 회원정보목록이 존재하면 JSON타입으로 변환 
-    }
+    // - 회원정보목록 호출 함수 실행 
+    let memberList = 회원정보목록반환함수();
 
     // 로그인정보 가 회원정보목록(기록)내 존재하는지 비교 
     for( let index = 0 ; index <= memberList.length-1 ; index++ ){
@@ -63,8 +46,25 @@ function 로그인함수(){
             return; // 현재 함수 종료 // 아래 코드는 실행되지 않는다.  
         }
     } // for end 
-
     alert('동일한 회원정보가 없습니다. 로그인실패');
-
     return;
 } // f end 
+
+// [3] 세션 에 있는 회원정보목록반환함수
+function 회원정보목록반환함수(){ // 매개변수x , 리턴값 : memberList
+    // * 회원목록정보를 전역변수에 선언하지 않고 세션/쿠키 호출 
+    // 왜? JS외부 (브라우저)에 저장하면 JS가 새로고침/변화가 있어도 저장 상태 유지 
+    // - (1) 현재 회원정보를 저장하기 위한 회원정보목록을 세션에서 가져오기.
+    let memberList = sessionStorage.getItem( 'memberList' );
+    console.log( memberList ); // 존재하지 않는 'key' 호출하면 null 반환된다.
+    // - (2) 회원정보목록이 존재하면 존재하지 않으면 
+    if( memberList == null ){
+        //만약에 가져온세션데이터(memberList)가 null(없다면) 이면
+        memberList = []; // 빈 배열 생성하여 변수에 대입한다. 
+    }else{// 가져온세션데이터(memberList) 가 존재하면 
+        // 문자열 ---> 객체(JSON) 으로 변환
+        memberList = JSON.parse( memberList );
+    }
+    return memberList; // 함수가 종료되면서 함수를 호출했던것으로 반환/리턴 하는 값 
+} // f end 
+
